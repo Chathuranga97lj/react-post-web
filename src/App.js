3c51cd3c5bd0f07953a17837b93d0025ef5d54eb
Chathuranga97lj <chathuit97@gmail.com>
2023-02-13 22:50:45 +0530

    customize nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import About from "./screens/About";
+import Contact from "./screens/Contact";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <About />
+      <Contact />
     </div>
   );
 }
